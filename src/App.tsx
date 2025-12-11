20ac839d11d16e034627c4222861dd5486b4ce35
equezda327 <enrique.a.quezada@gmail.com>
2025-12-11 02:54:22 -0800

    Installed CORS after initially forgetting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
-import AuthProvider, { AuthContext } from "./context/AuthProvider";
+import { AuthContext } from "./context/AuthProvider";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
@@ -8,18 +8,15 @@ import ProjectsPage from "./pages/ProjectsPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import "./App.css";
 
-// Protected Route wrapper component
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const authContext = useContext(AuthContext);
-
   if (!authContext?.user) {
     return <Navigate to="/auth" replace />;
   }
-
   return <>{children}</>;
 }
 
-function AppContent() {
+function App() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Navbar />
@@ -47,16 +44,6 @@ function AppContent() {
         </Routes>
       </main>
     </div>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <AppContent />
-      </AuthProvider>
-    </BrowserRouter>
   );
 }
 
